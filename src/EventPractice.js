eb5f0b8253ef91ac9018d60ca8d4d3688d1f7de2
Jiseong Kim <jiseong330@gmail.com>
2022-09-30 15:09:01 +0900

    Property Initializer Syntax(바벨의 transform-class-properties) 사용

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -4,17 +4,12 @@ class EventPractice extends Component {
     state = {
         message: '메세지'
     }
-    constructor(props){
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
-    handleChange(e) {
+    handleChange=(e)=>{
         this.setState({
             message: e.target.value
         });
     }
-    handleClick() {
+    handleClick=()=> {
         alert(this.state.message);
         this.setState({message:''});
     }
