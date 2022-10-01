f1885fba704b9915fa36535b56433eee1606d9b7
Jiseong Kim <jiseong330@gmail.com>
2022-10-01 16:21:49 +0900

    input에 ref 달기

--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -1,4 +1,3 @@
-import { findAllByTestId } from "@testing-library/react";
 import { Component } from "react";
 import './ValidationSample.css';
 
@@ -17,12 +16,14 @@ class ValidationSample extends Component {
         this.setState({
             clicked: true,
             validated: this.state.password === '0000'
-        })
+        });
+        this.input.focus();
     }
     render() {
         return (
             <div>
                 <input
+                    ref={(ref)=>this.input=ref}
                     type="password"
                     value={this.state.password}
                     onChange={this.handleChange}
