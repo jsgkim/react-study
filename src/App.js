78430be0cf43619ae5ff298c693525a2fc467400
Jiseong Kim <jiseong330@gmail.com>
2022-10-03 16:59:43 +0900

    LifeCycle 실습(랜덤 색상, 더하기)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import "./App.css";
-import IterationSample from "./IterationSample";
-
-const App = () => {
-  return <IterationSample></IterationSample>;
-};
+import { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+class App extends Component {
+  state = {color: '#000000'}
+  handleClick = () => {
+    this.setState({color: getRandomColor()})
+  }
+  render(){
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    )
+  }
+}
 
 export default App;
