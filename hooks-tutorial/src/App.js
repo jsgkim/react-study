718a5ef3d4ed9bbc1343c59c9239a3b9778704d4
Jiseong Kim <jiseong330@gmail.com>
2022-10-06 13:29:02 +0900

    useState

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Counter from "./Counter";
+import Info from "./Info";
+
+function App() {
+  return (
+    <>
+      <Counter />
+      <br />
+      <br />
+      <Info />
+    </>
+  );
+}
+
+export default App;
