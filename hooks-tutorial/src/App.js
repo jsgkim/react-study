73280b6857dfbe9771dca3a8c77e8100f8609cb4
Jiseong Kim <jiseong330@gmail.com>
2022-10-10 20:02:49 +0900

    useMemo

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,16 +1,8 @@
 import "./App.css";
-import { useState } from "react";
-import Info from "./Info";
+import Average from "./Average";
 
 function App() {
-  const [visible, setVisible] = useState(false);
-  return (
-    <>
-      <button onClick={()=> {setVisible(!visible);}}>{visible ? '숨기기':'보이기'}</button>
-      <hr />
-      {visible && <Info />}
-    </>
-  );
+  return <Average></Average>;
 }
 
 export default App;
