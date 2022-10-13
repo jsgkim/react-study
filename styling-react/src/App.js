0216335fe127c81957d7a2ffdc61335ce7b3144b
Jiseong Kim <jiseong330@gmail.com>
2022-10-13 21:13:08 +0900

    Sass 적용

---
+++ styling-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from 'react';
+import SassComponent from './SassComponent';
+
+function App() {
+  return (
+    <div>
+      <SassComponent></SassComponent>
+    </div>
+  );
+}
+
+export default App;
