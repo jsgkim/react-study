822168e291aeb57777300a953de28a137325e5fc
Jiseong Kim <jiseong330@gmail.com>
2022-10-17 21:36:11 +0900

    CSS Module 적용

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import {Component} from 'react';
-import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
 
 function App() {
   return (
     <div>
-      <SassComponent></SassComponent>
+      <CSSModule />
     </div>
   );
 }
