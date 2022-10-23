50dbd62c205ab99c9b6f6596e6fd4fa6a8a56e24
Jiseong Kim <jiseong330@gmail.com>
2022-10-23 11:19:48 +0900

    todo-app UI

---
+++ todo-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TodoInsert from './components/TodoInsert';
+import TodoTemplate from './components/TodoTemplate';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
+  )
+}
+
+export default App;
