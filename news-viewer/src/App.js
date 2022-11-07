e92bca08df0b940475f0061466b6701f65ab12f8
Jiseong Kim <jiseong330@gmail.com>
2022-11-07 18:31:10 +0900

    JSONPlaceholder API 요청

---
+++ news-viewer/src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import axios from '../node_modules/axios/index';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios.get('http://jsonplaceholder.typicode.com/todos/1').then(response => {
+      setData(response.data);
+    });
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+    </div>
+  );
+};
+
+export default App;
