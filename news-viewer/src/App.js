ff25e216caa913d7a73340fe64c678e1da98efd3
Jiseong Kim <jiseong330@gmail.com>
2022-11-09 20:13:17 +0900

    async, news api key 적용

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('http://jsonplaceholder.typicode.com/todos/1').then(response => {
+  const onClick = async () => {
+    try {
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&category=health&apiKey=463bf473be334a16aaa0938be8c64435');
       setData(response.data);
-    });
+    } catch (e) { console.log(e);}
   };
   return (
     <div>
