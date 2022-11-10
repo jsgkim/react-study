b15398a7c65adb8d045c4f2126e85b88ab21e0de
Jiseong Kim <jiseong330@gmail.com>
2022-11-10 15:34:39 +0900

    news-viewer UI

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,23 +1,7 @@
-import { useState } from 'react';
-import axios from '../node_modules/axios/index';
-import './App.css';
+import NewsList from './components/NewsList';
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&category=health&apiKey=463bf473be334a16aaa0938be8c64435');
-      setData(response.data);
-    } catch (e) { console.log(e);}
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
-};
+  return <NewsList />;
+}
 
 export default App;
