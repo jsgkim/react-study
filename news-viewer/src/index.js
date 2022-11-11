1ac23137f79af8e3b0feccaf4017ff0f7cfb3d88
Jiseong Kim <jiseong330@gmail.com>
2022-11-11 18:50:49 +0900

    라우터 적용

--- news-viewer/src/index.js
+++ news-viewer/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 );
