f36409d244192c30c5f7a8ba4bf5f1f594148db3
Jiseong Kim <jiseong330@gmail.com>
2022-11-17 13:31:48 +0900

    Provider, Redux DevTools 적용

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import App from './App';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, devToolsEnhancer());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 );
