d6237a5aa132764a4cd6298156527245581627a8
Jiseong Kim <jiseong330@gmail.com>
2022-11-20 18:52:29 +0900

    useActions, React.memo

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,6 +1,7 @@
-import { useCallback } from 'react';
-import { connect, useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 
 const TodosContainer = () => {
@@ -8,14 +9,11 @@ const TodosContainer = () => {
     input: todos.input,
     todos: todos.todos,
   }));
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback(
-    (input) => dispatch(changeInput(input)),
-    [dispatch],
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    [],
   );
-  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
 
   return (
     <Todos
@@ -29,4 +27,4 @@ const TodosContainer = () => {
   );
 };
 
-export default TodosContainer;
+export default React.memo(TodosContainer);
