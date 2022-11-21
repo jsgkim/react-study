08c0849ca923d85f9b19912561acbcfeb63e2ce6
Jiseong Kim <jiseong330@gmail.com>
2022-11-22 08:51:46 +0900

    Thunk 생성함수

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
 import App from "./App";
 import rootReducer from "./modules";
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
